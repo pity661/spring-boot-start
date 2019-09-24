11bcab70692f57552d5e338b01f1dbfb3e1993db
wenky <huwenqi@panda-fintech.com>
2019-09-24 14:12:21 +0800

    git rm -f --cache

---
+++ src/main/java/com/example/demo/domain/thread/synchronize/Customer4.java
@@ -0,0 +1,18 @@
+package com.example.demo.domain.thread.synchronize;
+
+/**
+ * @program: demo
+ * @description:
+ * @author: wenky huwenqi@panda-fintech.com
+ * @create: 2019-09-18 11:03
+ */
+public class Customer4 implements Runnable {
+  @Override
+  public void run() {
+    try {
+      Method.sub();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+}
